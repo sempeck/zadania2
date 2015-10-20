8a1ebd0a757605f986e27ea8a45bef1c4db6fd97
sempeck <jarek.sepek@mediorama.pl>
2015-10-20 19:10:38 +0200

    Zadanie 6

---
+++ zad6.pas
@@ -0,0 +1,23 @@
+// Napisz program, który sprawdza czy wprowadzona z klawiatury liczba całkowita jest liczbą kwadratową.
+
+program zad6;
+
+var 
+  x : Integer;
+  y : Real;
+
+begin
+	writeln('Program sprawdzi czy liczba jest kwadratowa.');
+	writeln('Podaj x:');
+	readln(x);
+
+    y := sqrt(abs(x));
+
+      if (y*y = x) then
+          writeln('Tak, podana liczba jest kwadratowa.') 
+      else
+    writeln('Niestety podana liczba nie jest kwadratowa.');
+
+    readln;
+
+end.
